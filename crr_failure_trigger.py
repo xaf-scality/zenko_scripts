34fd5ccc61ab2219a0139635fe22c7d9dd3b48f9
Jason Russler <jason.russler@gmail.com>
2019-05-03 11:41:16 -0400

    first post

---
+++ crr_failure_trigger.py
@@ -0,0 +1,120 @@
+#!/bin/python
+#
+# NOTES: the "retry-failed" cronjobs needs to be enabled for this script to
+#        work. In Zenko/vaules.yml you need:
+#
+#            maintanence.enabled = True
+#
+#        and you need to retain more than one passed successful run in the
+#        pod listing:
+#
+#            maintanance.successfulJobsHistory = 3
+#
+#        Then "helm upgrade" your deployment
+import subprocess
+import json
+import sys
+import os
+import time
+import smtplib
+from email.mime.text import MIMEText
+
+##
+# Configuration
+failure_threshold = 2 # Failures at or greater than this number result in an
+                      # alert. Needs to be the same or less than the retained
+                      # successfulJobsHistory value.
+failure_log_ret = 3   # Number of completed pods to expect for inspection
+ack_on_failure=True   # Send only one message per 'ack_timeout' value below
+ack_timout=86400
+kubectl_command_path = "/home/centos/zenko-stack/metalk8s-1.1.0-alpha1/.shell-env/metalk8s/bin/"
+kubectl_auth = "/home/centos/zenko-stack/metalk8s/inventory/galaxy-z/artifacts/admin.conf"
+email_on_failure=True
+email_recipient='centos@localhost'
+ack_file="/tmp/zenko_failures_ack"
+
+##
+# Set env
+subenv = os.environ.copy()
+subenv["PATH"] = "{0}:{1}".format(kubectl_command_path, os.environ["PATH"])
+subenv["KUBECONFIG"] = kubectl_auth
+
+message_buffer = ''
+
+def send_message(subject, message):
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['To'] = email_recipient
+    s = smtplib.SMTP('localhost')
+    s.sendmail(msg['From'], [email_recipient], msg.as_string())
+    s.quit()
+
+def do_something(subj, message):
+    if os.path.isfile(ack_file):
+        with open(ack_file, "r") as ft:
+            ack_t = int(ft.read())
+            try:
+                if ack_t < (int(time.time()) - ack_timout):
+                    send_message('ATTN: Probem with Zenko CRR Retries (repeat notice)')
+                    with open(ack_file, "w") as ft:
+                        ft.write(str(int(time.time())))
+                    return
+            except Exception as e:
+                 send_message(subj, 'problem with failure message script: {0}'.format(str(e)))   
+    else:
+        send_message(subj, message)
+        with open(ack_file, "w") as ft:
+            ft.write(str(int(time.time())))
+
+
+def check_retry_pods():
+    global message_buffer
+    ##
+    # Get the retry pod names
+    retry_pods_text = subprocess.Popen("kubectl get pods | grep retry-failed |grep Completed | gawk '{print $1}'",
+                        shell=True,
+                        env=subenv,
+                        stdout=subprocess.PIPE).stdout.read().strip()
+    retry_pods = retry_pods_text.split('\n')
+
+    failed_dests = {}
+    for pod in retry_pods:
+        try:
+            logtext = json.loads(subprocess.Popen("kubectl logs {0}".format(pod),
+                        shell=True,
+                        env=subenv,
+                        stdout=subprocess.PIPE).stdout.read().strip())
+        except Exception as e:
+            do_something('Problem checking retry pod logs',
+                    'problem with retry pod: {0}'.format(logtext['e']))
+            continue
+
+        if "error" in logtext:
+            do_something('Zenko Retry Job Error',
+                    'problem with retry pod: {0}'.format(logtext['error']))
+        else:
+            for dest in logtext['result']:
+                if not dest in failed_dests:
+                    failed_dests[dest] = 1
+                else:
+                    failed_dests[dest] += 1
+
+
+    for dest in failed_dests:
+        if failed_dests[dest] >= failure_threshold:
+            message_buffer = '''
+Please check the status of your Zenko cluster. A CRR failure threshold has
+been exceeded (or a monitoring script error has been encountered) for 
+destination:
+
+{0}
+
+Current configuration is to alert when {1} out of {2} CRR retry attempts fail.
+'''.format(dest, failure_threshold, failure_log_ret)
+            do_something('Too many CRR failures', message_buffer)
+    
+    return(True)
+
+if __name__ == "__main__":
+    
+    sys.exit(check_retry_pods())
---
+++ reset_redis.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import os
+import  argparse
+
+##
+# Configuration
+release = "galaxy-z"
+kubectl_command_path = "/home/centos/zenko-stack/metalk8s-1.1.0-alpha1/.shell-env/metalk8s/bin/"
+kubectl_auth = "/home/centos/zenko-stack/metalk8s/inventory/galaxy-z/artifacts/admin.conf"
+
+##
+# Set env
+subenv = os.environ.copy()
+subenv["PATH"] = "{0}:{1}".format(kubectl_command_path, os.environ["PATH"])
+subenv["KUBECONFIG"] = kubectl_auth
+
+def find_redis_master():
+    cmd = "kubectl get pods |grep {0} |grep redis".format(release)
+    cmd += "|gawk '{print $1}'"
+    redis_pods = subprocess.Popen(cmd, shell=True, env=subenv,
+                        stdout=subprocess.PIPE).stdout.read().strip().split('\n')
+
+    for pod in redis_pods:
+        cmd = 'redis-cli info|grep ^role |cut -d: -f2'
+        dcmd = 'kubectl exec {0} -c redis -- {1}  2> /dev/null'.format(pod, cmd)
+        role = subprocess.Popen(dcmd, shell=True, env=subenv,
+                        stdout=subprocess.PIPE).stdout.read().strip()
+        if role == 'master':
+            return pod
+
+def list_locations(master_pod):
+    
+    src_buckets = (),
+    dest_locations = ()
+
+    cmd = r"kubectl exec {0} -c redis -- redis-cli keys \*".format(master_pod)
+    r_keys = subprocess.Popen(cmd, shell=True, env=subenv,
+                        stdout=subprocess.PIPE).stdout.read().strip().split('\n')
+    for key in r_keys:
+        path = key.split(":")
+        if not path[1] in src_buckets:
+            src_buckets += (path[1],)
+        if not path[0] in dest_locations:
+            dest_locations += (path[0],)
+
+    print("\nThe following destination locations have keys in redis:")
+    for loc in dest_locations:
+        print(loc)
+
+def reset_pending(master_pod, dest):
+    bad = False
+    for k in ['bytespending', 'opspending']:
+
+        pending_key = "{0}:bb:crr:{1}".format(dest, k)
+        cmd = 'redis-cli get {0}'.format(pending_key)
+        dcmd = 'kubectl exec {0} -c redis -- {1}'.format(master_pod, cmd)
+        output = subprocess.Popen(dcmd, shell=True, env=subenv,
+                                stdout=subprocess.PIPE).stdout.read().strip()
+        
+        # Keep from making a mess out of redis
+        if output == '':
+            print('key ({0}) does not exist'.format(pending_key))
+            return
+
+        cmd = 'redis-cli set {0} 0'.format(pending_key)
+        dcmd = 'kubectl exec {0} -c redis -- {1}'.format(master_pod, cmd)
+        output = subprocess.Popen(dcmd, shell=True, env=subenv,
+                                stdout=subprocess.PIPE).stdout.read().strip()
+        if output != 'OK': bad == True
+
+    if bad == True: print('NOK')
+    else: print('OK')
+
+def reset_failed(master_pod, dest):
+    
+    keypattern = 'bb:crr:failed:{0}:*'.format(dest)
+    cmd = 'redis-cli keys {0}'.format(keypattern)
+    dcmd = 'kubectl exec {0} -c redis -- {1}'.format(master_pod, cmd)
+    keys = subprocess.Popen(dcmd, shell=True, env=subenv,
+                                stdout=subprocess.PIPE).stdout.read().strip().split('\n')
+    
+    for k in keys:
+        cmd = 'redis-cli del {0}'.format(k)
+        dcmd = 'kubectl exec {0} -c redis -- {1}'.format(master_pod, cmd)
+        output = subprocess.Popen(dcmd, shell=True, env=subenv,
+                                stdout=subprocess.PIPE).stdout.read().strip()
+        print("deleted keys: {0}".format(output))
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='reset failed/pending Zenko counters in redis USE WITH CARE')
+    parser.add_argument('--list', '-l', help='list available storage locations', action="store_true")
+    parser.add_argument('--failed', '-f', help='reset failed transfers to 0 for a location', default=False)
+    parser.add_argument('--pending', '-p', help='reset pending transfers to 0 for a location', default=False)
+    args = parser.parse_args()
+
+    master_pod = find_redis_master()
+    if not master_pod:
+        print("can't find a master")
+        sys.exit(1)
+    
+    if args.list:
+        list_locations(master_pod)
+    elif args.pending:
+        reset_pending(master_pod, args.pending)
+    elif args.failed:
+        reset_failed(master_pod, args.failed)
