ffe6ae32564aced6385b4f664c477b92dd3955af
Jason Russler <jason.russler@gmail.com>
2019-09-20 13:12:40 -0400

    just some formatting clean up

--- bucket_destruction.py
+++ bucket_destruction.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 #
 # Multi-session clean-up of S3 buckets. Hopefully fast, absolutly bug free. 
-
 import os
 import boto3
 import argparse
@@ -18,14 +17,13 @@ RETRIES=5
 
 def just_go(args):
     '''
-    Ever wonder why you create wrapper functions with only a few lines?
-    I don't.
+    Ever wonder why some people create wrapper functions with only a couple of
+    lines? I don't.
     '''
     zap_objects(args)
     if args.skipmpus != True: zap_mpus(args)
 
     return True
-
 
 def _run_batch(args, ovs):
     '''
@@ -43,7 +41,8 @@ def _run_batch(args, ovs):
             if args.noncurrent and ver["IsLatest"]:
                 logme("skipping {0} ({1}".format(ver['Key'], ver['VersionId']))
                 continue
-            objs['Objects'].append({'Key': ver['Key'], 'VersionId': ver['VersionId']})
+            objs['Objects'].append(
+                    {'Key': ver['Key'], 'VersionId': ver['VersionId']})
             logme("deleting {0} ({1}".format(ver['Key'], ver['VersionId']))
     except KeyError: pass
     except Exception as e:
@@ -55,8 +54,10 @@ def _run_batch(args, ovs):
 
         try:
             for ver in ovs["DeleteMarkers"]:
-                objs['Objects'].append({'Key': ver['Key'], 'VersionId': ver['VersionId']})
-                logme("deleting marker {0} ({1}".format(ver['Key'], ver['VersionId']))
+                objs['Objects'].append(
+                        {'Key': ver['Key'], 'VersionId': ver['VersionId']})
+                logme("deleting marker {0} ({1}".format(
+                                                ver['Key'], ver['VersionId']))
         except KeyError: pass
         except Exception as e:
             sys.stderr.write(str(e))
@@ -74,7 +75,6 @@ def _run_batch(args, ovs):
                 sys.stderr.write(str(e))
                 continue
             break
-    
 
 def zap_objects(args):
     '''
@@ -115,7 +115,8 @@ def zap_mpus(args):
             for mpu in mpus['Uploads']:
                 logme("deleting {0}".format(mpu['UploadId']))
                 if not args.dryrun:
-                    s3.abort_multipart_upload(Bucket=args.bucket, Key=mpu["Key"], UploadId=mpu['UploadId'])
+                    s3.abort_multipart_upload(Bucket=args.bucket, 
+                            Key=mpu["Key"], UploadId=mpu['UploadId'])
 
 def logme(text):
     ''' I know, right? '''
@@ -124,18 +125,26 @@ def logme(text):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Delete versions, markers and stray MPUs from an S3 a bucket. You want that bucket empty for removal, don\'t you?')
+    parser = argparse.ArgumentParser(description='Delete versions, markers and \
+stray MPUs from an S3 a bucket. You want that bucket empty for removal, don\'t \
+you?')
     parser.add_argument('--bucket', required=True)
     parser.add_argument('--profile', default=PROFILE_DEF)
     parser.add_argument('--endpoint', default='https://s3.amazonaws.com')
     parser.add_argument('--ca-bundle', default=False, dest='cabundle')
-    parser.add_argument('--prefix', default='', dest='prefix', help='delete at and beyond prefix')
-    parser.add_argument('--noncurrent', action='store_true', help='delete only non-current objects (and markers)')
-    parser.add_argument('--skipmarkers', action='store_true', help='skip deletion of delete markers')
-    parser.add_argument('--skipmpus', action='store_true', help='skip clean-up of unfinished MPUs')
-    parser.add_argument('--workers', default=WORKERS, help='number of workers to run (default: {0})'.format(WORKERS))
+    parser.add_argument('--prefix', default='', dest='prefix', help='delete at \
+and beyond prefix')
+    parser.add_argument('--noncurrent', action='store_true', help='delete only \
+non-current objects (and markers)')
+    parser.add_argument('--skipmarkers', action='store_true', help='skip \
+deletion of delete markers')
+    parser.add_argument('--skipmpus', action='store_true', help='skip clean-up \
+of unfinished MPUs')
+    parser.add_argument('--workers', default=WORKERS, help='number of workers \
+to run (default: {0})'.format(WORKERS))
     parser.add_argument('--quiet', action='store_true', help='supress output')
-    parser.add_argument('--dryrun', action='store_true', help='show objects to be deleted but don\'t (useless with --quiet)')
+    parser.add_argument('--dryrun', action='store_true', help='show objects to \
+be deleted but don\'t (useless with --quiet)')
     args = parser.parse_args()
 
     # No idea why boto can't get this via API
